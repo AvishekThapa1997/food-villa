b12c1849e65decee5efe0faaecf62710f407d8fb
avishek.thapa <avishek.thapa@mindfiresolutions.com>
2023-01-13 20:30:52 +0530

    restaurant filteration added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from './components/Container';
 import Header from './components/Header';
 import RestaurantList from './components/RestaurantList';
 import SearchBox from './components/SearchBox';
 import { restaurantData } from './config/constants';
+import useDebounce from './util/debounce';
+import { searchRestaurant } from './util/util';
 
 const App = () => {
+	const [, setSearchParam] = useState('');
+	const [restaurants, setRestaurants] = useState(restaurantData);
+	function _filterRestaurant(params) {
+		setSearchParam(params);
+		const filteredRestaurants = searchRestaurant(restaurantData, params);
+		setRestaurants(filteredRestaurants);
+	}
+	const filterRestaurant = useDebounce(_filterRestaurant, 300);
 	return (
 		<>
 			<Header />
 			<main>
 				<Container>
-					<SearchBox />
-					<RestaurantList restaurantData={restaurantData} />
+					<SearchBox filterRestaurant={filterRestaurant} />
+					<RestaurantList restaurantData={restaurants} />
 				</Container>
 			</main>
 		</>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { searchBox, searchInput } from './SearchBox.css';
-const SearchBox = () => {
+const SearchBox = ({ filterRestaurant }) => {
+	const inputChangeHandler = (event) => {
+		const value = event.target.value;
+		filterRestaurant(value);
+	};
 	return (
 		<div className={searchBox}>
 			<input
 				type='text'
 				className={searchInput}
 				placeholder='search restaurant'
+				onChange={inputChangeHandler}
 			/>
 		</div>
 	);
