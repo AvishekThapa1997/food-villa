dda719d1202ac2c7116a80d778c99b1bbbb0fa87
avishek.thapa <avishek.thapa@mindfiresolutions.com>
2023-01-13 18:12:12 +0530

    add container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Container from './components/Container';
 import Header from './components/Header';
 
 const App = () => {
 	return (
 		<>
 			<Header />
+			<main>
+				<Container></Container>
+			</main>
 		</>
 	);
 };
